75f0af899d47398a945c0d35636eddc648c96b55
lizaq94 <domagala.k94@gmail.com>
2021-12-01 12:46:56 +0100

    Remove unnecessary comments

--- src/components/atoms/BarItem/BarItem.js
+++ src/components/atoms/BarItem/BarItem.js
@@ -1,38 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// export const Wrapper = styled.div`
-//   flex: 1;
-//   max-width: 19.35rem;
-// `;
-// export const BarItemContent = styled.div`
-//   width: 100%;
-//   height: 4.5rem;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   cursor: pointer;
-//   border-radius: 1.6rem;
-//   color: ${({ isActive }) => (isActive ? 'white' : 'black')};
-//   background-color: ${({ isActive }) => (isActive ? ({ theme }) => theme.colors.primaryBlue : '#f2f2f2')};
-// `;
-//
-// export const StyledBarItemContent = styled(BarItemContent)`
-//   color: black;
-//   background-color: ${({ isActive }) => (isActive ? ({ theme }) => theme.colors.white : '#f2f2f2')};
-// `;
-
-// const Wrapper = styled.div`
-//   flex: 1;
-//
-//   input {
-//     display: none;
-//   }
-//
-//   input:checked ~ label {
-//     background: red;
-//   }
-// `;
 const Label = styled.label`
   padding: 8px 0px;
 
